49778aed2140f9c65c6b282d508885d56987f0fb
Echelan <andres_movilla1103@hotmail.com>
2018-01-05 13:46:18 -0500

    added project

---
+++ Project/src/artificialgeneticalgorithm/AGA.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package artificialgeneticalgorithm;
+
+import java.util.Random;
+
+/**
+ *
+ * @author Andrés Movilla
+ */
+public class AGA {
+    
+    public static Random r;
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+	r = new Random();
+	
+	new Thread(new Handler()).start();
+    }
+    
+    
+    
+}
